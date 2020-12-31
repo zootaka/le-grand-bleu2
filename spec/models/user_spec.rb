6b248537d1b8ddc1cb164e3e5bb4d291f2dfb6c5
zootaka <69644312+zootaka@users.noreply.github.com>
2020-12-31 14:21:44 +0900

    userモデルのテストコード作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    context 'ユーザー登録がうまくいく時' do
+      it 'nickname,nameとemail、passwordとpassword_confirmationが存在すれば登録できること' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context 'ユーザー登録がうまくいかない時' do
+      it 'nicknameが空では登録できないこと' do
+        @user.nickname = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+
+      it 'emailが空では登録できないこと' do
+        @user.email = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+
+      it 'passwordが空では登録できないこと' do
+        @user.password = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it 'passwordが6文字以上であれば登録できること' do
+        @user.password = '111111'
+        @user.password_confirmation = '111111'
+        expect(@user).to be_valid
+      end
+
+      it 'passwordが5文字以下であれば登録できないこと' do
+        @user.password = '11111'
+        @user.password_confirmation = '11111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+
+      it 'passwordとpassword_confirmationが不一致では登録できないこと' do
+        @user.password = '111111'
+        @user.password_confirmation = '222222'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it '重複したemailが存在する場合登録できないこと' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+    end
+  end
 end
