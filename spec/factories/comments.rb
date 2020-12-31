dae04dd853f67313708a981ba26e85c0bdabb541
zootaka <69644312+zootaka@users.noreply.github.com>
2020-12-31 14:38:31 +0900

    コメントテストコード作成

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :comment do
-    
+    factory :comment do
+      content { Faker::Lorem.sentence }
+      association :user
+      association :map_place
+  
+      after(:build) do |comment|
+        comment.image.attach(io: File.open('public/images/testimage.jpeg'), filename: 'testimage.jpeg')
+      end
+    end
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @comment = FactoryBot.build(:comment)
+    end
+
+    it 'contentとimageが存在していれば保存できること' do
+      expect(@comment).to be_valid
+    end
+
+    it 'imageが空でも保存できること' do
+      @comment.image = nil
+      expect(@comment).to be_valid
+    end
+
+    it 'contentとimageが空では保存できないこと' do
+      @comment.content = nil
+      @comment.image = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Content can't be blank")
+    end
+
+    it 'roomが紐付いていないと保存できないこと' do
+      @comment.map_place = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include('Map place must exist')
+    end
+
+    it 'userが紐付いていないと保存できないこと' do
+      @comment.user = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include('User must exist')
+    end
+  end
 end
+
