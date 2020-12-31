918528e23f629c804c130d992ab6427d8cd1fa15
zootaka <69644312+zootaka@users.noreply.github.com>
2020-12-31 14:45:57 +0900

    コメントテーブルとコントローラの作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,39 @@
 class BoardsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+  before_action :set_spot, only: [:index, :create]
+
+  def index
+    @comment = Comment.new
+    @comments = @spot.comments.all
+  end
+
+  def create
+    @comment = @spot.comments.new(comment_params)
+    if @comment.save
+      redirect_to map_boards_path(@spot)
+    else
+      @comments = @spot.comments.all
+      render :index
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if current_user.id == @comment.user_id
+      @comment.destroy
+      redirect_to map_boards_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_spot
+    @spot = Spot.find(params[:map_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :image).merge(user_id: current_user.id, spot_id: params[:map_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "maps#index"
-  resources :maps, only: [:index, :create] 
+  
+  resources :maps, only: [:index, :create] do
+    resources :boards, only [:index, :create, :destroy]
+  end
+
   resources :users, only: [:index, :edit, :update, :destroy]
 end
