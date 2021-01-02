f81c25de23bf2048be0a7defd7a52379aa40a835
zootaka <69644312+zootaka@users.noreply.github.com>
2021-01-02 14:16:36 +0900

    rogbook単体テストコード作成

--- spec/models/log_book_spec.rb
+++ spec/models/log_book_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe LogBook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @log_book = FactoryBot.build(:log_book)
+    end
+
+    context 'ログブック登録がうまくいく時' do
+      it '全てのカラムが存在すれば登録できること' do
+        expect(@log_book).to be_valid
+      end
+    end
+
+    context 'ログブック登録がうまくいかない時' do
+      it 'placeが空では登録できないこと' do
+        @log_book.place = nil
+        @log_book.valid?
+        expect(@log_book.errors.full_messages).to include("Place can't be blank")
+      end
+    end
+  end
 end
