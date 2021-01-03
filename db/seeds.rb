dfefb20cd7c739807e79f60374ac3716428ff31c
zootaka <69644312+zootaka@users.noreply.github.com>
2021-01-03 23:29:53 +0900

    管理者権限

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(nickname:  "管理者",
+  email: "admin@example.jp",
+  password:  "11111111",
+  password_confirmation: "11111111",
+  admin: true)
+
+MapPlace.create!(
+  place_name: "柏島",
+  latitude: "32.7712109",
+  longitude: "132.6292793"
+)
