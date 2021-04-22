fdac1ad15b4f32379a4bb56d83e215047771e582
zootaka <69644312+zootaka@users.noreply.github.com>
2021-04-22 16:33:12 +0900

    pumaの本番環境の設定

--- config/puma.rb
+++ config/puma.rb
@@ -36,3 +36,19 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+# アプリケーションディレクトリ
+app_dir = File.expand_path("../../", __FILE__)
+# ソケット通信を図る為bindでURI指定
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+# PIDファイル所在(プロセスID)
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+# stateファイルはpumactlコマンドでサーバーを操作する。その所在。
+state_path "#{app_dir}/tmp/pids/puma.state"
+# 標準出力/標準エラーを出力するファイルの所在。
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
+#port       ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+plugin :tmp_restart
