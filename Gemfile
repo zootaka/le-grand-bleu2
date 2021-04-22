3b78042dd65c0d0b381b24f07e27763f85574995
zootaka <69644312+zootaka@users.noreply.github.com>
2021-04-22 23:01:05 +0900

    puma変更

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'rubocop', require: false
+ 
 end
 
 group :test do
--- config/puma.rb
+++ config/puma.rb
@@ -47,8 +47,4 @@ pidfile "#{app_dir}/tmp/pids/puma.pid"
 state_path "#{app_dir}/tmp/pids/puma.state"
 # 標準出力/標準エラーを出力するファイルの所在。
 stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
-#port       ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
-plugin :tmp_restart
+
